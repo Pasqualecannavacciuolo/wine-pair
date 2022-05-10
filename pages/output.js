e70574e86a9fae7c5605addf19734a2abb714b22
Pasqualecannavacciuolo <p.cannavacciuolo@icloud.com>
2022-05-10 18:57:36 +0200

    Ignore

--- pages/output.js
+++ pages/output.js
@@ -6,8 +6,8 @@ import { createClient } from '@supabase/supabase-js'
 export default function Output() {
     const router = useRouter();
 
-    const supabaseUrl = process.env.SUPABASE_URL;
-    const supabaseKey = process.env.SUPABASE_KEY;
+    const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+    const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
     const supabase = createClient(supabaseUrl, supabaseKey);
 
     const [ricerca, setRicerca] = useState(null);
