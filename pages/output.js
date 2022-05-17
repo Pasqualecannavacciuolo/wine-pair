94b1b71b788de08f837a3464c6f70f7aaaf6ad62
Pasqualecannavacciuolo <p.cannavacciuolo@icloud.com>
2022-05-17 10:27:25 +0200

    Improvements and commenting

--- pages/output.js
+++ pages/output.js
@@ -11,7 +11,6 @@ import {
     Text,
     Stack,
     SimpleGrid,
-    useColorModeValue,
     Button,
 } from '@chakra-ui/react';
 
@@ -28,15 +27,7 @@ export default function Output() {
 
 
     useEffect(() => {
-        getRicerca()
-        /*fetch('http://localhost:5000/ricerca/1')
-            .then(response => {
-                return response.json();
-            })
-            .then((data) => {
-                setRicerca(data)
-            });*/
-
+        getRicerca();
     }, []);
 
     async function getRicerca() {
@@ -66,13 +57,6 @@ export default function Output() {
 
     useEffect(() => {
         getRossoLeggero();
-        /*fetch('http://localhost:5000/rosso_leggero')
-            .then(response => {
-                return response.json();
-            })
-            .then((data) => {
-                setPiattiRossoLeggero(data);
-            });*/
     }, []);
 
     async function getRossoLeggero() {
@@ -96,13 +80,6 @@ export default function Output() {
 
     useEffect(() => {
         getRossoStrutturato();
-        /*fetch('http://localhost:5000/rosso_strutturato')
-            .then(response => {
-                return response.json();
-            })
-            .then((data) => {
-                setPiattiRossoStruturato(data);
-            });*/
     }, []);
 
     async function getRossoStrutturato() {
@@ -126,13 +103,6 @@ export default function Output() {
 
     useEffect(() => {
         getBiancoLeggero();
-        /*fetch('http://localhost:5000/bianco_leggero')
-            .then(response => {
-                return response.json();
-            })
-            .then((data) => {
-                setPiattiBiancoLeggero(data);
-            });*/
     }, []);
 
     async function getBiancoLeggero() {
@@ -156,13 +126,6 @@ export default function Output() {
 
     useEffect(() => {
         getBiancoStrutturato();
-        /*fetch('http://localhost:5000/bianco_strutturato')
-            .then(response => {
-                return response.json();
-            })
-            .then((data) => {
-                setPiattiBiancoStruturato(data);
-            });*/
     }, []);
 
     async function getBiancoStrutturato() {
@@ -184,7 +147,7 @@ export default function Output() {
         }
     }
 
-
+    // Getting all the dishes for every wine and course
     const antipasto_rosso_leggero = piatti_rosso_leggero && piatti_rosso_leggero.antipasto;
     const primo_rosso_leggero = piatti_rosso_leggero && piatti_rosso_leggero.primo;
     const secondo_rosso_leggero = piatti_rosso_leggero && piatti_rosso_leggero.secondo;
@@ -202,92 +165,51 @@ export default function Output() {
     const secondo_bianco_strutturato = piatti_bianco_strutturato && piatti_bianco_strutturato.secondo;
 
     let output = new Set();
-    let vino_rosso_leggero = {
-        id: '',
-        image: '',
-        nome: '',
-        descrizione: ''
-    }
-    let vino_rosso_strutturato = {
-        id: '',
-        image: '',
-        nome: '',
-        descrizione: ''
-    }
-    let vino_bianco_leggero = {
-        id: '',
-        image: '',
-        nome: '',
-        descrizione: ''
-    }
-    let vino_bianco_strutturato = {
-        id: '',
-        image: '',
-        nome: '',
-        descrizione: ''
-    }
 
+    // This object contains the basic information of a wine
+    const vino = function(id, image, nome, descrizione){
+        let vino_generale = {};
+        vino_generale.id = id;
+        vino_generale.image = image;
+        vino_generale.nome = nome;
+        vino_generale.descrizione = descrizione;
+        return vino_generale;
+    }
+    
+    // Searching the input in the dishes of every courses for every wine
     if (antipasto_rosso_leggero && primo_rosso_leggero && secondo_rosso_leggero) {
+        let vino_rosso_leggero = new vino(new Date().getTime() + Math.random(), '/images/rosso-leggero.jpeg', "Rosso Leggero", "Un vino leggero da accompagnare a piatti con gusti delicati non troppo travolgenti");
         for (let i = 0; i < antipasto_rosso_leggero.length; i++) {
             if (antipasto_rosso_leggero[i] === antipasto) {
-                //console.log("Uguali antipasti")
-                vino_rosso_leggero.id = (new Date()).getTime() + Math.random()
-                vino_rosso_leggero.image = '/images/rosso-leggero.jpeg';
-                vino_rosso_leggero.nome = "Rosso Leggero";
-                vino_rosso_leggero.descrizione = "Un vino leggero da accompagnare a piatti con gusti delicati non troppo travolgenti"
                 output.add(vino_rosso_leggero)
             }
         }
         for (let j = 0; j < primo_rosso_leggero.length; j++) {
             if (primo_rosso_leggero[j] === primo) {
-                //console.log("Uguali primi")
-                vino_rosso_leggero.id = (new Date()).getTime() + Math.random()
-                vino_rosso_leggero.image = '/images/rosso-leggero.jpeg';
-                vino_rosso_leggero.nome = "Rosso Leggero";
-                vino_rosso_leggero.descrizione = "Un vino leggero da accompagnare a piatti con gusti delicati non troppo travolgenti"
                 output.add(vino_rosso_leggero)
             }
         }
         for (let i = 0; i < secondo_rosso_leggero.length; i++) {
             if (secondo_rosso_leggero[i] === secondo) {
-                //console.log("Uguali secondi")
-                vino_rosso_leggero.id = (new Date()).getTime() + Math.random()
-                vino_rosso_leggero.image = '/images/rosso-leggero.jpeg';
-                vino_rosso_leggero.nome = "Rosso Leggero";
-                vino_rosso_leggero.descrizione = "Un vino leggero da accompagnare a piatti con gusti delicati non troppo travolgenti"
-                output.add(vino_rosso_leggero)
+                output.add(vino_rosso_leggero);
             }
         }
     }
 
     if (antipasto_rosso_strutturato && primo_rosso_strutturato && secondo_rosso_strutturato) {
+        let vino_rosso_strutturato = new vino(new Date().getTime() + Math.random(), '/images/rosso-strutturato.jpeg', "Rosso Strutturato", "Un vino strutturato da accompagnare a piatti con gusti forti e decisi per godersi al meglio la cena");
         for (let i = 0; i < antipasto_rosso_strutturato.length; i++) {
             if (antipasto_rosso_strutturato[i] === antipasto) {
-                //console.log("Uguali antipasti")
-                vino_rosso_strutturato.id = (new Date()).getTime() + Math.random()
-                vino_rosso_strutturato.image = '/images/rosso-strutturato.jpeg';
-                vino_rosso_strutturato.nome = "Rosso Strutturato";
-                vino_rosso_strutturato.descrizione = "Un vino strutturato da accompagnare a piatti con gusti forti e decisi per godersi al meglio la cena"
-                output.add(vino_rosso_strutturato)
+                output.add(vino_rosso_strutturato);
             }
         }
         for (let j = 0; j < primo_rosso_strutturato.length; j++) {
             if (primo_rosso_strutturato[j] === primo) {
-                //console.log("Uguali primi")
-                vino_rosso_strutturato.id = (new Date()).getTime() + Math.random()
-                vino_rosso_strutturato.image = '/images/rosso-strutturato.jpeg';
-                vino_rosso_strutturato.nome = "Rosso Strutturato";
-                vino_rosso_strutturato.descrizione = "Un vino strutturato da accompagnare a piatti con gusti forti e decisi per godersi al meglio la cena"
                 output.add(vino_rosso_strutturato)
             }
         }
         for (let i = 0; i < secondo_rosso_strutturato.length; i++) {
             if (secondo_rosso_strutturato[i] === secondo) {
-                //console.log("Uguali secondi")
-                vino_rosso_strutturato.id = (new Date()).getTime() + Math.random()
-                vino_rosso_strutturato.image = '/images/rosso-strutturato.jpeg';
-                vino_rosso_strutturato.nome = "Rosso Strutturato";
-                vino_rosso_strutturato.descrizione = "Un vino strutturato da accompagnare a piatti con gusti forti e decisi per godersi al meglio la cena"
                 output.add(vino_rosso_strutturato)
             }
         }
@@ -295,60 +217,38 @@ export default function Output() {
 
 
     if (antipasto_bianco_leggero && primo_bianco_leggero && secondo_bianco_leggero) {
+        let vino_bianco_leggero = new vino(new Date().getTime() + Math.random(), '/images/bianco-leggero.png', "Bianco Leggero","Un vino leggero da accompagnare a piatti con gusti delicati non troppo travolgenti");
         for (let i = 0; i < antipasto_bianco_leggero.length; i++) {
             if (antipasto_bianco_leggero[i] === antipasto) {
-                vino_bianco_leggero.id = (new Date()).getTime() + Math.random()
-                vino_bianco_leggero.image = '/images/bianco-leggero.png';
-                vino_bianco_leggero.nome = "Bianco Leggero";
-                vino_bianco_leggero.descrizione = "Un vino leggero da accompagnare a piatti con gusti delicati non troppo travolgenti"
                 output.add(vino_bianco_leggero)
             }
         }
         for (let j = 0; j < primo_bianco_leggero.length; j++) {
             if (primo_bianco_leggero[j] === primo) {
-                vino_bianco_leggero.id = (new Date()).getTime() + Math.random()
-                vino_bianco_leggero.image = '/images/bianco-leggero.png';
-                vino_bianco_leggero.nome = "Bianco Leggero";
-                vino_bianco_leggero.descrizione = "Un vino leggero da accompagnare a piatti con gusti delicati non troppo travolgenti"
                 output.add(vino_bianco_leggero)
             }
         }
         for (let i = 0; i < secondo_bianco_leggero.length; i++) {
             if (secondo_bianco_leggero[i] === secondo) {
-                vino_bianco_leggero.id = (new Date()).getTime() + Math.random()
-                vino_bianco_leggero.image = '/images/bianco-leggero.png';
-                vino_bianco_leggero.nome = "Bianco Leggero";
-                vino_bianco_leggero.descrizione = "Un vino leggero da accompagnare a piatti con gusti delicati non troppo travolgenti"
                 output.add(vino_bianco_leggero)
             }
         }
     }
 
     if (antipasto_bianco_strutturato && primo_bianco_strutturato && secondo_bianco_strutturato) {
+        let vino_bianco_strutturato = new vino(new Date().getTime() + Math.random(), '/images/bianco-strutturato.jpeg', "Bianco Strutturato","Un vino strutturato da accompagnare a piatti con gusti forti e decisi per godersi al meglio la cena");
         for (let i = 0; i < antipasto_bianco_strutturato.length; i++) {
             if (antipasto_bianco_strutturato[i] === antipasto) {
-                vino_bianco_strutturato.id = (new Date()).getTime() + Math.random()
-                vino_bianco_strutturato.image = '/images/bianco-strutturato.jpeg';
-                vino_bianco_strutturato.nome = "Bianco Strutturato";
-                vino_bianco_strutturato.descrizione = "Un vino strutturato da accompagnare a piatti con gusti forti e decisi per godersi al meglio la cena"
                 output.add(vino_bianco_strutturato)
             }
         }
         for (let j = 0; j < primo_bianco_strutturato.length; j++) {
             if (primo_bianco_strutturato[j] === primo) {
-                vino_bianco_strutturato.id = (new Date()).getTime() + Math.random()
-                vino_bianco_strutturato.image = '/images/bianco-strutturato.jpeg';
-                vino_bianco_strutturato.nome = "Bianco Strutturato";
-                vino_bianco_strutturato.descrizione = "Un vino strutturato da accompagnare a piatti con gusti forti e decisi per godersi al meglio la cena"
                 output.add(vino_bianco_strutturato)
             }
         }
         for (let i = 0; i < secondo_bianco_strutturato.length; i++) {
             if (secondo_bianco_strutturato[i] === secondo) {
-                vino_bianco_strutturato.id = (new Date()).getTime() + Math.random()
-                vino_bianco_strutturato.image = '/images/bianco-strutturato.jpeg';
-                vino_bianco_strutturato.nome = "Bianco Strutturato";
-                vino_bianco_strutturato.descrizione = "Un vino strutturato da accompagnare a piatti con gusti forti e decisi per godersi al meglio la cena"
                 output.add(vino_bianco_strutturato)
             }
         }
@@ -361,11 +261,6 @@ export default function Output() {
     console.log(final)
 
     const backToHome = async () => {
-        /*const response = await fetch('http://localhost:5000/ricerca/1', {
-             method: 'DELETE'
-         });
-         const data = await response.json();
-         console.log(data);*/
         const { data, error } = await supabase
             .from('ricerca')
             .delete()
